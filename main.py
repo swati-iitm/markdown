1945dd8121a240c4415228fb69e2c0ae1a8f8994
swati-iitm <23ds3000185@ds.study.iitm.ac.in>
2025-02-08 23:44:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def get_wiki(country: str = Query(default='India')):
     #country = 'India'
     #return country
     url = f"https://en.wikipedia.org/wiki/{country}"
-    return url
+    #return url
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.5",
