185672b5dd929881cab1f4c3ef64e6c77357a56c
swati-iitm <23ds3000185@ds.study.iitm.ac.in>
2025-02-08 23:04:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 )
 
 @app.get("/")
-def get_wikipedia_outline(country: Optional[List[str]] = Query(default=[]):
+def get_wikipedia_outline(country: Optional[List[str]] = Query(default=['India']):
     url = f"https://en.wikipedia.org/wiki/{country}"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
